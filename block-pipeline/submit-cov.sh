658295c9d3873c2a48d03923f4f2dc33fe8fda53
Timothy Raben <traben13@gmail.com>
2024-06-24 15:34:22 -0400

    Update submit-cov.sh
    Cleaned out debugging comments

--- block-pipeline/submit-cov.sh
+++ block-pipeline/submit-cov.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
 trait=$1
-workPATH="/mnt/home/rabentim/metric-projection/${trait}/"
-#workPATH="/mnt/home/rabentim/sparse-methods/${trait}/"
-#workPATH="/mnt/home/rabentim/snp-projection/${trait}/"
-traits=("gout" "hgt" "CAD" "BMI" "asthma" "diabetes.type1" "diabetes.type2" "hypertension" "hyperlip-cont" "Direct.bilirubin" "Lipoprotein.A" "atrial.fibrillation")
+workPATH="/FULL/WORKING/PATH"
+traits=("LIST" "OF" "PHENOTYPE" "NAMES")
 
 for trait in ${traits[@]}; do
     sbatch cov.sh $trait
 done
 wait
-
-#sbatch lasso.sh
