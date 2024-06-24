c22c181a6ceb894b443903b5fb71400ca9617591
Timothy Raben <traben13@gmail.com>
2024-06-24 15:27:24 -0400

    Update score-lasso.sh
    added comments for running

--- block-pipeline/score-lasso.sh
+++ block-pipeline/score-lasso.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH -n 1 -c 10
-#SBATCH --time=3:59:00
-#SBATCH --mem=256G
-#SBATCH --job-name=outscore
-#SBATCH --output=%x-%a-%A.SLURMout
-#SBATCH -a 1-5
+#SBATCH SPECIFY SLURM COMMANDS (OR OTHER WORKLOAD MANAGER)
 
-module purge
-module load GCC/8.3.0 OpenMPI/3.1.4 imkl/2019.5.281
-module load Python/3.8.3
-source '/mnt/home/rabentim/programs/pysnptools/bin/activate'
-k=$SLURM_ARRAY_TASK_ID
-echo $k
+#load modules
 
+##########
 traitname=$1
 echo $traitname
 m=$2
@@ -20,20 +11,15 @@ s=$3
 c=$4
 ML='LASSO'
 echo $ML
+#specify validation set size
 valN=2500
-#valN=250
-OUTDIR='/mnt/home/rabentim/methods'/$traitname/
-#OUTDIR='/mnt/home/rabentim/snp-projection'/$traitname/
-#OUTDIR='/mnt/home/rabentim/metric-projection'/$traitname/
-#OUTDIR='/mnt/home/rabentim/taiwan/qc'/$traitname/
-genoPATH='/mnt/research/UKBB/hsuGroup/ukb500/genotypes/calls.merged/ukb500.calls.onlyqc'
-#genoPATH='/mnt/scratch/rabentim/taiwan/twb2calls-ukbimp/ukb.twn.overlap.qc'
-cohortDIR='/mnt/research/UKBB/hsuGroup/ukb500/cohorts/'
+OUTDIR='FULL/WOKRING/PATH'
+
+genoPATH='PATH/TO/BED/FILES'
+#cohorts (e.g. ancestry groups, sibling groups, etc.) are identified beforehand
+cohortDIR='PATH/TO/COHORT/FILES'
 gwastype='CACO'
-#gwastype='PCA_YOB_SEX_reg'
-#covtype='PCA_YOB_reg'
-#covtype='YOB_SEX_reg'
-#covtype='PCA_YOB_reg'
+#covariates adjusted for (for file naming conventions)
 covtype='PCA_YOB_SEX_reg'
 
 python score-ML.py --geno-path $genoPATH \
