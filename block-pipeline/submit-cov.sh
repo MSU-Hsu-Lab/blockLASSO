67251d5773ab8ac9c9c392aaca527686d7ff796f
Timothy Raben <traben13@gmail.com>
2024-06-24 15:36:04 -0400

    Update submit-cov.sh
    replaced single cov submission with block cov submission

--- block-pipeline/submit-cov.sh
+++ block-pipeline/submit-cov.sh
@@ -2,9 +2,15 @@
 
 trait=$1
 workPATH="/FULL/WORKING/PATH"
-traits=("LIST" "OF" "PHENOTYPE" "NAMES")
 
-for trait in ${traits[@]}; do
-    sbatch cov.sh $trait
+mapfile -t S < ${workPATH}sets/snp_sizes.txt
+#specifying memory and time requirements for SLURM submission
+mems=(16 16 16 16 16 32 32 64 64 128 256)
+tims=(00:59:00 00:59:00 00:59:00 00:59:00 00:59:00 00:59:00 00:59:00 00:59:00 02:59:00 5:59:00 11:59:00)
+
+tmp=${#S[@]}
+
+for s in $(seq 0 $((tmp-1))); do
+    sbatch --mem=${mems[$s]}G --time=${tims[$s]} perc-cov.sh $trait ${S[$s]}
 done
 wait
